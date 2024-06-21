cafbc6e1a21b5e45be8992242fb6583a267ae481
Jordan Christensen <33182552+JordanChristensen@users.noreply.github.com>
2024-06-21 14:28:16 -0700

    init components and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/NavBar/Navbar";
+import MailboxList from "./components/MailboxList/MailboxList";
+import MailboxForm from "./components/MailboxForm/MailboxForm";
+import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <main>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" />
+        <Route path="/mailboxes" />
+        <Route path="/new-mailbox" />
+        <Route path="/mailboxes/:mailboxId" />
+      </Routes>
+      <h1>Hello world!</h1>
+      <MailboxList />
+      <MailboxForm />
+      <MailboxDetails />
+    </main>
+  );
 };
 
 export default App;
