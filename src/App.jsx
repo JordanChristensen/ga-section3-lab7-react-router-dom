f5cbf1d8463d2f24a13ab22bd5d4c28bfd5cb235
Jordan Christensen <33182552+JordanChristensen@users.noreply.github.com>
2024-06-22 11:09:53 -0700

    implement react-error-boundary, store init state in variable

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+import * as manageMailboxes from "./utils/manageMailboxes";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import MailboxList from "./components/MailboxList/MailboxList";
 import MailboxForm from "./components/MailboxForm/MailboxForm";
 import MailboxDetails from "./components/MailboxDetails/MailboxDetails";
 
-const App = () => {
+function Fallback({ error }) {
+  return <section>Something went wrong: {error.message}</section>;
+}
+
+function Mailbox() {
+  const [mailboxes, setMailboxes] = useState(initMailboxState);
+
+  function handleNewMailbox() {
+    manageMailboxes.addBox;
+  }
+
   return (
     <main>
       <Navbar />
-
       <Routes>
         <Route
           path="/"
@@ -38,6 +50,23 @@ const App = () => {
       </Routes>
     </main>
   );
+}
+
+const initMailboxState = {
+  _id: null,
+  boxSize: "",
+  boxOwner: "",
 };
 
-export default App;
+export default function App() {
+  return (
+    <ErrorBoundary
+      FallbackComponent={Fallback}
+      onError={(error, errorInfo) => {
+        console.error("React-error-boundary caught an error", error, errorInfo);
+      }}
+    >
+      <Mailbox />
+    </ErrorBoundary>
+  );
+}
