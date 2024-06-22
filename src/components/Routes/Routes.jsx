22a886774ef5e796f412b48ce8941fd7d48a3e3d
Jordan Christensen <33182552+JordanChristensen@users.noreply.github.com>
2024-06-22 13:36:02 -0700

    remove dead code

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,4 +1,3 @@
-// import React from "react";
 import { Route, Routes } from "react-router-dom";
 import MailboxList from "../MailboxList/MailboxList";
 import MailboxForm from "../MailboxForm/MailboxForm";
