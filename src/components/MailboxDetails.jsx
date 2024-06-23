927c9e5903cbc2495b16377bbff6c4ad4d893277
Jordan Christensen <33182552+JordanChristensen@users.noreply.github.com>
2024-06-23 06:29:38 -0700

    fix error: use conditional to assign mapped object to local object

--- src/components/MailboxDetails.jsx
+++ src/components/MailboxDetails.jsx
@@ -1,41 +1,18 @@
 import { useParams } from "react-router-dom";
 
 export default function MailboxDetails({ mailboxes }) {
-  //
-  //
-  // ----------------
-  // ----------------
   const thisMailboxId = useParams();
-  // WHYYYY ---> is boxSize and boxHolder..... NOT AVAILABLE??
-  console.log("URL params provides: ", thisMailboxId);
-  console.log(...mailboxes);
-  mailboxes.map((mailbox) => {
-    if (mailbox._id === thisMailboxId) {
-      console.log(mailbox.boxholder);
+  let thisMailbox = {};
+
+  mailboxes?.map((mailbox) => {
+    if (mailbox._id === Number(thisMailboxId.mailboxId)) {
+      thisMailbox = { ...mailbox };
     }
   });
-  // ----------------
-  // ----------------
-  //
-  //   const thisMailbox = mailboxes.find(
-  //     (mailbox) => mailbox._id === Number(thisMailboxId)
-  //   );
-  //   console.log(thisMailbox);
-  //
-  // ----------------
-  // ----------------
-  //
-  //   console.log("thisMailboxID ", thisMailboxId);
-  //   mailboxes.map((mailbox) => {
-  //     console.log(mailbox._id, thisMailboxId);
-  //     if (Number(mailbox._id) === Number(thisMailboxId)) {
-  //       <h1>WINNER</h1>;
-  //     }
-  //   });
 
   return (
     <section>
-      <h1>Mailbox {mailboxes.thisMailboxId}</h1>
+      <h1>Mailbox {thisMailbox._id}</h1>
     </section>
   );
 }
