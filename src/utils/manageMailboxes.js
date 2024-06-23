aed3e9deb9125e571e3523bab339c5793ddf09fc
Jordan Christensen <33182552+JordanChristensen@users.noreply.github.com>
2024-06-22 17:21:44 -0700

    remove dead code, update notes

--- src/utils/manageMailboxes.js
+++ src/utils/manageMailboxes.js
@@ -1,9 +1,7 @@
-export async function addBox({
-  newMailboxFormData,
-  mailboxes,
-  setMailboxes,
-}) {
+export async function addBox({ newMailboxFormData, mailboxes, setMailboxes }) {
   try {
+    console.log(newMailboxFormData);
+    // console.log(newMailboxFormData);
     await setMailboxes([...mailboxes, { ...newMailboxFormData }]);
   } catch (setMailboxesErr) {
     console.log("Error when creating new mailbox ----->", setMailboxesErr);
